d3d80be0c850e7281758f50fd1a5cd012788a442
Robert Varela <rvarela@utexas.edu>
2016-12-07 11:19:57 -0600

    show average of temps list

--- src/containers/weatherlist.js
+++ src/containers/weatherlist.js
@@ -12,52 +12,61 @@ class WeatherList extends Component {
     const tempsK = cityData.list.map(weather => weather.main.temp);
     const tempsF = tempsK.map( array => Math.round(array * (9/5) - 459.67));
     const dates = cityData.list.map(date => date.dt_txt.split(" ")[0]);
-    const descriptionArray = cityData.list.map(des => des.weather);
-    const description = descriptionArray.map(data => data[0].description);
-
     const noDuplicateDatesArray = dates.filter(function(elem, index, self) {
       return index == self.indexOf(elem);
     });
-    const averageTempsArray = [];
-    const indexOfArray = [];
-    let tempValueOfTemps = [];
+    const cityDateTemps = {};
 
-    for (var i = 0; i < dates.length; i++) {
-      if (dates[i] == noDuplicateDatesArray[0]) {
-        indexOfArray.push(i)
-      }
+  dates.map((date,index) => {
+    if(cityDateTemps.hasOwnProperty(date)){
+      cityDateTemps[date].daytemps.push(tempsF[index]);
+      cityDateTemps[date].avg = average(cityDateTemps[date].daytemps);
+    } else {
+      cityDateTemps[date] = {};
+      cityDateTemps[date].daytemps = [];
+      cityDateTemps[date].daytemps.push(tempsF[index]);
+      cityDateTemps[date].avg = average(cityDateTemps[date].daytemps);
     }
+  });
 
-    for (var i = 0; i < tempsF.length; i++) {
-      for (var j = 0; j < indexOfArray.length; j++){
-        if (indexOfArray[j] == i) {
-          tempValueOfTemps.push(tempsF[i]);
-        }
-      }
-    }
+  function average (data) {
+    return _.round(_.sum(data)/data.length);
+  }
 
-    function average (data) {
-      averageTempsArray.push(_.round(_.sum(data)/data.length));
-    }
+  const dateTemps = Object.keys(cityDateTemps);
+  const avgArray = dateTemps.map((date)=> cityDateTemps[date].avg);
 
-    average(tempValueOfTemps);
+  console.log(avgArray);
 
-    console.log(tempsF);
-    console.log(tempValueOfTemps);
-    console.log(averageTempsArray);
-    console.log(tempValueOfTemps);
+    // const descriptionArray = cityData.list.map(des => des.weather);
+    // const description = descriptionArray.map(data => data[0].description);
+
+    // const averageTempsArray = [];
+    // const indexOfArray = [];
+    // let tempValueOfTemps = [];
+    // for (var i = 0; i < dates.length; i++) {
+    //   if (dates[i] == noDuplicateDatesArray[0]) {
+    //     indexOfArray.push(i)
+    //   }
+    // }
+    // for (var i = 0; i < tempsF.length; i++) {
+    //   for (var j = 0; j < indexOfArray.length; j++){
+    //     if (indexOfArray[j] == i) {
+    //       tempValueOfTemps.push(tempsF[i]);
+    //     }
+    //   }
+    // }
+
+
 
     // const pressures = cityData.list.map(weather => weather.main.pressure);
     // const humidities = cityData.list.map(weather => weather.main.humidity);
     // const lon = cityData.city.coord.lon;
     // const lat = cityData.city.coord.lat;
 
-    // console.log(tempsF);
-    // console.log(description);
-
     return<tr key={name}>
       <td>{name}</td>
-      <td> <Forecast temps={averageTempsArray} dates={noDuplicateDatesArray} des={description}/> </td>
+      <td> <Forecast temps={avgArray} dates={noDuplicateDatesArray}/> </td>
       {/* <td> <GoogleMap lon={lon} lat={lat} /> </td>
       <td><Chart data={tempsF} color="orange" units="F"/></td>
       <td><Chart data={pressures} color="green" units="hPa"/></td>
